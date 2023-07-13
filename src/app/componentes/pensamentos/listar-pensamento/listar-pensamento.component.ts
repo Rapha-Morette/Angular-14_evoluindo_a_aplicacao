30933fd9136621305203af8eab8e6bec4c669f92
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-07-13 15:45:57 -0300

    Módulo 3 concluído

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,19 +5,28 @@ import { Pensamento } from '../pensamento';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
 
-  constructor(private service: PensamentoService) { }
+  haMaisPensamentos: boolean = true;
+
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
-
