59aaa23ecfb718298bfa5b4765af6ec1416732e1
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-07-14 14:02:53 -0300

    Módulo 4 video 1 concluído

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,6 +12,7 @@ export class ListarPensamentoComponent implements OnInit {
   paginaAtual: number = 1;
 
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) {}
 
